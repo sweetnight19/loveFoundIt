ac4f22e0ca39e8a21fe20ea3282afa8fdd237653
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2021-12-20 20:24:06 +0100

    Created color constants and added primary and secondary colors

---
+++ lib/color_constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color PRIMARY_BUTTON_BACKGROUND_COLOR = Colors.pink;
+const Color PRIMARY_BUTTON_FONT_COLOR = Colors.white;
+
+const Color SECONDARY_BUTTON_BACKGROUND_COLOR = Colors.white;
+const Color SECONDARY_BUTTON_FONT_COLOR = Colors.black;
+
